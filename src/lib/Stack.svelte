1f6b80d970df345c71fe499219a0abe9eed85f07
FomasTreeman <tom@team-freeman.com>
2023-08-10 14:19:31 +0100

    stack refactor dup code

--- src/lib/Stack.svelte
+++ src/lib/Stack.svelte
@@ -1,51 +1,13 @@
+<script>
+	import StackIcons from './StackIcons.svelte';
+</script>
+
 <div class="scroll-parent">
 	<div class="scroll-element primary icons">
-		<i class="devicon-react-original colored" />
-		<i class="devicon-git-plain colored" />
-		<i class="devicon-bash-plain colored" />
-		<i class="devicon-svelte-plain colored" />
-		<i class="devicon-postgresql-plain colored" />
-		<i class="devicon-linux-plain colored" />
-		<i class="devicon-docker-plain colored" />
-		<i class="devicon-electron-original colored" />
-		<i class="devicon-express-original colored" />
-		<i class="devicon-heroku-original colored" />
-		<i class="devicon-html5-plain colored" />
-		<i class="devicon-css3-plain colored" />
-		<i class="devicon-jetbrains-plain colored" />
-		<i class="devicon-jquery-plain colored" />
-		<i class="devicon-linux-plain colored" />
-		<i class="devicon-nodejs-plain colored" />
-		<i class="devicon-python-plain colored" />
-		<i class="devicon-ssh-original colored" />
-		<i class="devicon-threejs-original colored" />
-		<i class="devicon-vim-plain colored" />
-		<i class="devicon-bootstrap-plain colored" />
-		<i class="devicon-figma-plain colored" />
+		<StackIcons />
 	</div>
 	<div class="scroll-element secondary icons">
-		<i class="devicon-react-original colored colored" />
-		<i class="devicon-git-plain colored" />
-		<i class="devicon-bash-plain colored" />
-		<i class="devicon-svelte-plain colored" />
-		<i class="devicon-postgresql-plain colored" />
-		<i class="devicon-linux-plain colored" />
-		<i class="devicon-docker-plain colored" />
-		<i class="devicon-electron-original colored" />
-		<i class="devicon-express-original colored" />
-		<i class="devicon-heroku-original colored" />
-		<i class="devicon-html5-plain colored" />
-		<i class="devicon-css3-plain colored" />
-		<i class="devicon-jetbrains-plain colored" />
-		<i class="devicon-jquery-plain colored" />
-		<i class="devicon-linux-plain colored" />
-		<i class="devicon-nodejs-plain colored" />
-		<i class="devicon-python-plain colored" />
-		<i class="devicon-ssh-original colored" />
-		<i class="devicon-threejs-original colored" />
-		<i class="devicon-vim-plain colored" />
-		<i class="devicon-bootstrap-plain colored" />
-		<i class="devicon-figma-plain colored" />
+		<StackIcons />
 	</div>
 </div>
 
