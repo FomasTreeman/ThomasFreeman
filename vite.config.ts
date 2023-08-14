72bacfcc0c284db17bc828a710c7d7dcbd3d15d0
FomasTreeman <tom@team-freeman.com>
2023-08-14 20:48:22 +0100

    vite config cahnge

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-});
+	},// attempt to fix .glb static file
+	resolve: {
+		alias: {
+			$img: mode === "production" ? "./static/img" : "../img",
+		},
+	},
+}));
