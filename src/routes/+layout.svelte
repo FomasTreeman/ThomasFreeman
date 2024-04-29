1989291ab42a339ea515d0747b90a90e85f14287
Thomas Freeman <tom@team-freeman.com>
2024-04-29 18:15:12 +0100

    remove tracker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,8 @@
 	import Background from '$lib/Background.svelte';
 	import Header from '../lib/Header.svelte';
 	import Footer from '../lib/Footer.svelte';
-	import { SpeedInsights } from "@vercel/speed-insights/next"
 </script>
 
-<SpeedInsights/>
 <Background />
 <Header />
 <slot />
